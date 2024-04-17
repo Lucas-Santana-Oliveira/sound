cb75b6ad55954d698ca6be7c06af3252944ae1d8
l3c4s <lucassantanapgsp@gmail.com>
2024-04-16 21:45:04 -0300

    atualizado

--- src/data.js
+++ src/data.js
@@ -1,116 +1,127 @@
 //image imports
-import peter from './img/peter.jpg';
-import stewie from './img/stewie.png';
-import quag from './img/quag.png';
-import brian from './img/brian.png';
-import lois from './img/lois.png';
-import meg from './img/meg.png';
-import chris from './img/chris.png';
-import joe from './img/joe.png';
-import consuella from './img/consuella.png';
-import death from './img/death.png';
-import cleve from './img/cleve.png';
-import herbert from './img/hebert.jpg';
+import edward from './img/edward.jpg';
+import naruto from './img/naruto.jpg';
+import luffy from './img/luffy.jpg';
+import saitama from './img/saitama.jpg';
+import goku from './img/goku.png';
+import vegeta from './img/vegeta.jpg';
+import yuji from './img/yuji.jpg';
+import yusuke from './img/yusuke.jpg';
 
-//peter sounds
-import idea from './sounds/peter/an_idea.mp3';
-import crazy from './sounds/peter/areyou_crazy.mp3';
-import arh from './sounds/peter/arhh_peter.mp3';
-import beat from './sounds/peter/beatyou.mp3';
-import fine from './sounds/peter/fine.mp3';
-import bird from './sounds/peter/bird_peter.mp3';
+// Edward sounds
+import bravo from './sounds/edward/bravo.mp4';
+import feijao from './sounds/edward/feijao.mp4';
+import projeto from './sounds/edward/projeto.mp4';
+import pulgar from './sounds/edward/pulgar.mp4';
+import vermelho from './sounds/edward/vermelho.mp4';
 
-//consuela sounds
-import afuera from './sounds/consuella/afuera.mp3';
-import broken from './sounds/consuella/broken.mp3';
-import mo_lemon from './sounds/consuella/mo_lemon.mp3';
-import no from './sounds/consuella/no-4.mp3';
-import ok from './sounds/consuella/ok.mp3';
-import rain from './sounds/consuella/rain.mp3';
+// Goku sounds
+import eusou from './sounds/goku/eusou.mp4';
+import fracassado from './sounds/goku/fracassado.mp4';
+import furioso from './sounds/goku/furioso.mp4';
+import ideias from './sounds/goku/ideias.mp4';
+import nojo from './sounds/goku/nojo.mp4';
 
-//herbert sounds
-import convo_starter from './sounds/herbert/convo_starter.mp3';
-import excuse_me from './sounds/herbert/excuse_me.mp3';
-import little_man from './sounds/herbert/little_man.mp3'
+// Naruto sounds
+import comoé from './sounds/naruto/comoé.mp4';
+import esqueceu from './sounds/naruto/esqueceu.mp4';
+import louca from './sounds/naruto/louca.mp4';
+import nãogosto from './sounds/naruto/não gosto.mp4';
+import taradinho from './sounds/naruto/taradinho.mp4';
 
-//quagmire sounds
-import arhh from './sounds/quag/arhh.mp3';
-import fatchicks from './sounds/quag/fatchicks.mp3';
-import giggidy from './sounds/quag/giggidy.mp3';
-import lookatme from './sounds/quag/lookatme.mp3';
-import no_prob from './sounds/quag/no_prob.mp3';
+// Luffy sounds
+import camelo from './sounds/luffy/camelo.mp4';
+import cerejeira from './sounds/luffy/cerejeira.mp4';
+import chinela from './sounds/luffy/chinela.mp4';
+import épedraé from './sounds/luffy/épedraé.mp4';
+import logica from './sounds/luffy/logica.mp4';
 
-//lois sounds
-import fuck_you from './sounds/lois/fu.mp3';
-import gotosleep from './sounds/lois/gotosleep.mp3';
-import hello from './sounds/lois/hello.mp3';
-import lost_mind from './sounds/lois/lost_mind.mp3';
-import ohno from './sounds/lois/ohno.mp3';
+// Saitama sounds
+import ata from './sounds/saitama/ata.mp4';
+import carcereira from './sounds/saitama/carcereira.mp4';
+import louça from './sounds/saitama/Louça.mp4';
+import nº2 from './sounds/saitama/nº2.mp4';
+import trave from './sounds/saitama/trave.mp4';
 
-//stewie sounds
-import brian_shout from './sounds/stewie/brian.mp3';
-import donottellme from './sounds/stewie/donottellme.mp3';
-import im_stewie from './sounds/stewie/im_stewie.mp3';
-import puke from './sounds/stewie/puke.mp3';
-import you_suck from './sounds/stewie/you_suck.mp3';
+// Vegeta sounds
+import bomhumor from './sounds/vegeta/bomhumor.mp4';
+import bulma from './sounds/vegeta/bulma.mp4';
+import classico from './sounds/vegeta/classico.mp4';
+import encher from './sounds/vegeta/encher.mp4';
+import idiota from './sounds/vegeta/idiota.mp4';
+import puro from './sounds/vegeta/puro.mp4';
+
+// Yuji sounds
+import bestofrindo from './sounds/yuji/bestofrindo.mp4';
+import entendi from './sounds/yuji/entendi.mp4';
+import okay from './sounds/yuji/okay.mp4';
+import vambora from './sounds/yuji/vambora.mp4';
+import VanDaine from './sounds/yuji/VanDaine.mp4';
+
+// Yusuke sounds
+import altura from './sounds/yusuke/altura.mp4';
+import fome from './sounds/yusuke/fome.mp4';
+import penalte from './sounds/yusuke/penalte.mp4';
+import Popó from './sounds/yusuke/Popó.mp4';
+import samba from './sounds/yusuke/samba.mp4';
 
 const data = [
-        {
-            id: 1,
-            name: 'Peter',
-            image: peter,
-            sounds: [idea, crazy, arh, beat, fine, bird],
-            soundNames: ['an idea', 'are you crazy', 'arhh', 'beat you', 'fine', 'bird']
-        },
-        {
-            id: 2,
-            name: 'Stewie',
-            image: stewie,
-            sounds: [brian_shout, donottellme, im_stewie, puke, you_suck],
-            soundNames: ['brian', 'dont tell me', 'im stewie', 'puke', 'you suck']
-        },
-        {
-            id: 3,
-            name: 'Quagmire',
-            image: quag,
-            sounds: [arhh, fatchicks, giggidy, lookatme, no_prob],
-            soundNames: ['arhh', 'fat chicks', 'giggidy', 'dont look at me like that', 'no problem']
-        },
-        {
-            id: 4,
-            name: 'Lois',
-            image: lois,
-            sounds: [fuck_you, gotosleep, hello, lost_mind, ohno],
-            soundNames: ['fuck you', 'go to sleep', 'hello', 'lost my mind', 'oh no']
-        },
-        {
-            id: 5,
-            name: 'Herbert',
-            image: herbert,
-            sounds: [convo_starter, excuse_me, little_man],
-            soundNames: ['convo starter','excuse me','little man']
-        },
-        {
-            id: 6,
-            name: 'Consuella',
-            image: consuella,
-            sounds: [afuera,broken,mo_lemon,no,ok,rain],
-            soundNames: ['afuera','broken','mo lemon','no','ok','rain']
-        },
-        {
-            id: 7,
-            name: 'Meg',
-            image: meg,
-            sounds: [],
-            soundNames: []
-        },
-        {
-            id: 8,
-            name: 'Chris',
-            image: chris,
-            sounds: [],
-            soundNames: []
-        },
-]
+    {
+        id: 1,
+        name: 'edward',
+        image: edward,
+        sounds: [bravo, feijao, projeto, pulgar, vermelho],
+        soundNames: ['bravo', 'feijao', 'projeto', 'pulgar', 'vermelho']
+    },
+    {
+        id: 2,
+        name: 'naruto',
+        image: naruto,
+        sounds: [comoé, esqueceu, louca, nãogosto, taradinho],
+        soundNames: ['comoé', 'esqueceu', 'louca', 'não gosto', 'taradinho']
+    },
+    {
+        id: 3,
+        name: 'luffy',
+        image: luffy,
+        sounds: [camelo, cerejeira, chinela, épedraé, logica],
+        soundNames: ['camelo', 'cerejeira', 'chinela', 'épedraé', 'logica']
+    },
+    {
+        id: 4,
+        name: 'saitama',
+        image: saitama,
+        sounds: [ata, carcereira, louça, nº2, trave],
+        soundNames: ['ata', 'carcereira', 'louça', 'nº2', 'trave']
+    },
+    {
+        id: 5,
+        name: 'yusuke',
+        image: yusuke,
+        sounds: [altura, fome, penalte, Popó, samba],
+        soundNames: ['altura', 'fome', 'penalte', 'Popó', 'samba']
+    },
+    {
+        id: 6,
+        name: 'yuji',
+        image: yuji,
+        sounds: [bestofrindo, entendi, okay, vambora, VanDaine],
+        soundNames: ['bestofrindo', 'entendi', 'okay', 'vambora', 'VanDaine']
+    },
+    {
+        id: 7,
+        name: 'goku',
+        image: goku,
+        sounds: [eusou, fracassado, furioso, ideias, nojo],
+        soundNames: ['eusou', 'fracassado', 'furioso', 'ideias', 'nojo']
+    },
+    {
+        id: 8,
+        name: 'vegeta',
+        image: vegeta,
+        sounds: [bomhumor, bulma, classico, encher, idiota, puro],
+        soundNames: ['bomhumor', 'bulma', 'classico', 'encher', 'idiota', 'puro']
+    }
+];
 
 export default data;
